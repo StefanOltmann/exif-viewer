f65958bf65267327e2173ac399ac5a411a5009c4
Stefan Oltmann <github@stefan-oltmann.de>
2024-04-01 17:26:49 +0200

    Update to Kim v0.17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ kotlin {
         val wasmJsMain by getting
 
         commonMain.dependencies {
-            api("com.ashampoo:kim:0.16.4")
+            api("com.ashampoo:kim:0.17")
         }
 
         commonTest.dependencies {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,7 +14,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        mavenLocal()
     }
 }
 
