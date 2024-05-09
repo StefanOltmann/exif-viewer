3a89dc4234bc8ee544e60754c329e1a987ca00df
Stefan Oltmann <github@stefan-oltmann.de>
2024-05-09 23:56:00 +0200

    Update to Kim v0.18.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ kotlin {
         val wasmJsMain by getting
 
         commonMain.dependencies {
-            api("com.ashampoo:kim:0.18")
+            api("com.ashampoo:kim:0.18.1")
         }
 
         commonTest.dependencies {
